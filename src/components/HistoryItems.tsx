5edf10ed48b8d7c7ad9dfb0b77b4a5dcb0a2cb41
Dev-ZiA <mohdkhasif622@gmail.com>
2023-09-08 16:43:44 +0500

    Expense tracker

--- src/components/HistoryItems.tsx
+++ src/components/HistoryItems.tsx
@@ -3,10 +3,19 @@ const HistoryItems = () => {
         <>
         <div className="container">
             <li className="plus">
-                Salary <span>+$400</span><button className="delete-btn">x</button>
+                Salary <span>+$800</span><button className="delete-btn">x</button>
+            </li>
+            <li className="plus">
+                Penssion <span>+$800</span><button className="delete-btn">x</button>
             </li>
             <li className="minus">
                 Rent <span>-$400</span><button className="delete-btn">x</button>
+            </li>
+            <li className="minus">
+                Shopping <span>+$500</span><button className="delete-btn">x</button>
+            </li>
+            <li className="minus">
+                Food <span>+$100</span><button className="delete-btn">x</button>
             </li>
         </div>
         </>
